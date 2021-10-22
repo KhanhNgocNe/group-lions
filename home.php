1a134256fc3ff4768ea4e9454b294250432867eb
ngocdkgcc19167 <91665971+ngocdkgcc19167@users.noreply.github.com>
2021-10-22 16:21:50 +0700

    Update home.php

--- home.php
+++ home.php
@@ -12,11 +12,13 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   </head>
-  <body>
+
   <?php
           session_start();
           include("connection.php");
       ?>
+  <body>
+  
     <div class="bg-1">
     <div class="container">
       <div class="row text-center">
