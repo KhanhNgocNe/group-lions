aecb0e764a0ce247bb760f21d6b7825c71a81e51
ngocdkgcc19167 <91665971+ngocdkgcc19167@users.noreply.github.com>
2021-10-22 16:53:52 +0700

    commit

--- conecttion.php
+++ conecttion.php
@@ -1,6 +1,5 @@
 <?php
-$conn = pg_connect("postgres://pyrbvjruwxpqxc:b1fef120a3288ab103ee74d680ae719836e4ec20b928fbc84f6e7490e765f451@ec2-18-235-4-83.compute-1.amazonaws.com:5432/d4947qoaqcqf5l");
-	echo 'Connected Successfully!!!';
+$conn = pg_connect("postgres://pyrbvjruwxpqxc:b1fef120a3288ab103ee74d680ae719836e4ec20b928fbc84f6e7490e765f451@ec2-18-235-4-83.compute-1.amazonaws.com:5432/d4947qoaqcqf5l")
 	if(!$conn)
 	{
 		die("Could not connect to database");
--- home.php
+++ home.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <title> Exercise 1 </title>
     <meta charset = " utf - 8 ">
     <meta name = " viewport " content = " width = device - width , initial - scale = 1 ">
     <link rel="stylesheet" href="style.css">
@@ -12,154 +12,13 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   </head>
-  <?php
-  session_start();
-include_once("connection.php");
-?>
+   <?php
+          session_start();
+          include("connection.php");
+      ?>
   <body>
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-      </ol>
-  
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="image/lebabo.discover.jpg" alt="Image">
-          <div class="carousel-caption">
-          </div>      
-        </div>
-  
-        <div class="item">
-          <img src="image/mcm.gif" alt="Image">
-          <div class="carousel-caption">
-            
-          </div>      
-        </div>
 
-        <div class="item">
-          <img src="image/louisvuitton.png" alt="Image">
-          <div class="carousel-caption">
-          </div>      
-        </div>
-
-        <div class="item">
-          <img src="image/lv.gif" alt="Image">
-          <div class="carousel-caption">
-            <h3>More Sell $</h3>
-            <p>Lorem ipsum...</p>
-          </div> 
-      </div>
-  
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-  </div>
-</div>
-
-<div class="container-fluid bg-3 text-center">
-  <div class="col-md-12">
-    <div class="row">
-    <div class="col-sm-3">
-        <div class="product">
-            <p>All Product</p>
-            <img src="image/allproduct.jpg" class="image" style="width:100%" alt="Image">
-            <div class="middle">
-            <a href="?page=allproduct"><button class="btn">Shop Now</button></a>
-            </div>
-        </div>
-      </div>
-      <div class="col-sm-3">
-        <div class="product">
-            <p>MCM</p>
-            <img src="image/mcm.perfume.jpg" class="image" style="width:100%" alt="Image">
-            <div class="middle">
-            <a href="?page=mcm"><button class="btn">Shop Now</button></a>
-            </div>
-        </div>
-      </div>
-          <div class="col-sm-3">
-            <div class="product">
-            <p>Dior</p>
-            <img src="image/dior.perfume.png" class="image" style="width:100%" alt="Image">
-            <div class="middle">
-            <a href="?page=dior"><button class="btn">Shop Now</button></a>
-          </div>
-          </div>
-        </div>
-          <div class="col-sm-3">
-            <div class="product">
-            <p>Gucci</p>
-            <img src="image/gucci.perfume.jpg" class="image" style="width:100%" alt="Image">
-            <div class="middle">
-            <a href="?page=gucci"><button class="btn">Shop Now</button></a>
-          </div>
-        </div>
-      </div>
-    </div>
-      <div class="row">
-        <div class="col-sm-3">
-          <div class="product">
-            <p>Louis Vuitton</p>
-              <img src="image/lv.perfume.jpg"class="image" style="width:100%" alt="Image">
-              <div class="middle">
-              <a href="?page=lv"><button class="btn">Shop Now</button></a>
-          </div>
-        </div>
-      </div>
-          <div class="col-sm-3"> 
-            <div class="product">
-            <p>LeLaBo</p>
-            <img src="image/lelabo.perfume.jpg" class="image" style="width:100%" alt="Image">
-            <div class="middle">
-            <a href="?page=lelabo"><button class="btn">Shop Now</button></a>
-            </div>  
-          </div>
-        </div>
-          <div class="col-sm-3">
-            <div class="product">
-            <p>Chanel</p>
-            <img src="image/chanel.perfume.jpg" class="image" style="width:100%" alt="Image">
-            <div class="middle">
-            <a href="?page=chanel" ><button class="btn">Shop Now</button></a>
-          </div>
-        </div>
-      </div>
-        <div class="col-sm-3">
-            <div class="product">
-            <p>Orther Product</p>
-            <img src="image/ortherproduct.jpg" class="image" style="width:100%" alt="Image">
-            <div class="middle">
-            <a href="?page=ortherproduct"><button class="btn">Shop Now</button></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-        <p class="brand">Shop by brand</p>
-          <div class="row">
-            <div class="col-sm-12">
-              <div class="well well-sm" style="background: white;">
-                <img src="image/lv.png"  style="width:5%" alt="Image">
-                <img src="image/gucci.jpg"  style="width:5%" alt="Image"> 
-                <img src="image/chanel.jpg"  style="width:5%" alt="Image">
-                <img src="image/lebabo.jpg"  style="width:5%" alt="Image">
-                <img src="image/mcm.jpg" style="width:3%" alt="Image">
-              </div>
-            </div>
-          </div>
-        </div>
-</div>
-</div>
-<div class="bg-1">
+    <div class="bg-1">
     <div class="container">
       <div class="row text-center">
       <?php
@@ -169,7 +28,7 @@ include_once("connection.php");
           if (!$result) { //add this check.
             die('Invalid query: ' . pg_error($conn));
                         }
-          while($row = pg_fetch_array($result,Null, PGSQL_ASSOC)){
+          while($row = pg_fetch_array($result,Null , PGSQL_ASSOC)){
         ?>
         <div class="col-sm-3">
           <div class="thumbnail" style="background: #F2F2F2;">
@@ -185,5 +44,6 @@ include_once("connection.php");
         </div>
     </div>
 </div>
+
 </body>
 </html>
